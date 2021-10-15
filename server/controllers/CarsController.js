dce758d10686de1685b5217a687c8a30baf65819
Danny Brown <danny.brown@upstart.com>
2021-10-14 21:30:04 -0400

    remaining endpoints

--- server/controllers/CarsController.js
+++ server/controllers/CarsController.js
@@ -2,7 +2,7 @@ const express = require('express')
 const db = require('./../../data/db-config.js')
 const router = express.Router();
 
-// GET cars
+// GET cars list
 router.get('/', async (req, res) => {
     try {
         const cars = await db.find();
@@ -17,6 +17,7 @@ router.get('/', async (req, res) => {
     }
 })
 
+// GET car by id
 router.get('/:id', async (req, res) => {
     try {
         const cars = await db.findById(req.params.id)
@@ -38,7 +39,7 @@ router.get('/:id', async (req, res) => {
     }
 })
 
-// POST: create
+// POST create car
 router.post('/', async (req, res) => {
     try {
         const cars = await db.insert(req.body)
@@ -56,6 +57,48 @@ router.post('/', async (req, res) => {
         console.log(err);
         return res.status(500).json({
             error: 'There was an error while saving the car to the DB'
+        })
+    }
+})
+
+// PUT updated/edit car
+router.put('/:id', async (req, res) => {
+    try {
+        const cars = await db.update(req.params.id, req.body)
+
+        if (cars) {
+            return res
+                .status(200)
+                .json(cars)
+        }
+
+        return res
+            .status(404)
+            .json()
+
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            error: 'The car data could not be modified.'
+        })
+    }
+})
+
+// DELETE
+router.delete('/:id', async (req, res) => {
+    try {
+        const cars = await db.remove(req.params.id)
+
+        if (cars.length > 0) {
+            return res.status(201).json(cars)
+        }
+
+        return res.status(404).json({
+            message: 'The car with the specified ID does not exist'
+        })
+    } catch (err) {
+        return res.status(500).json({
+            error: ' The car could not be removed'
         })
     }
 })
