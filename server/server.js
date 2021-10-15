27892a3e0ae9d02bd650adc4b4b6115f14e31d4d
Danny Brown <danny.brown@upstart.com>
2021-10-14 21:02:22 -0400

    cars list endpoint

--- server/server.js
+++ server/server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const server = express()
+const db = require('./../data/db-config.js')
 
 server.use(express.json())
 
 server.get('/', (req, res) => {
     res.send('<h1>Welcome to the Molly\'s Server</h1>')
 })
+
+server.get('/cars', async (req, res) => {
+        try {
+            const cars = await db.find();
+            return res
+                .status(200)
+                .json(cars)
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({
+                error: "These are not the cars you are looking for."
+            });
+        }
+    })
 
 module.exports = server;
